2b93ecd8b3dbd263cf99c2c2b13dc92f296c6130
gayathrirathnasamy <83351476+gayathrirathnasamy@users.noreply.github.com>
2021-04-30 18:40:58 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+const debounce = (fn, limit) => {
+  let timeout;
+  return function () {
+    let context = this;
+    let args = arguments;
+    clearTimeout(timeout); //every call cleartimout
+    timeout = setTimeout(() => {
+      fn.apply(context, args);
+    }, limit);
+  };
+};
+
+const throttle = (fn, limit) => {
+  let inThrottle = true;
+  return function () {
+    let context = this;
+    let args = arguments;
+    if (inThrottle) {
+      fn.apply(context, args); //calls once for 1st time since inThrottle set to true & works only on continuous user interaction
+      inThrottle = false;
+      setTimeout(() => {
+        inThrottle = true;
+      }, limit);
+    }
+  };
+};
+
+document.getElementById("btn").addEventListener(
+  "click",
+  debounce(function () {
+    console.log("Printing");
+  }, 1000)
+);
